8e836f333f3996930cc0d1a85680db675818cf69
Hans Hoffman <hoffman.hans.j@gmail.com>
2021-06-19 18:44:39 -0400

    chore: update nix config

--- shell.nix
+++ shell.nix
@@ -3,17 +3,16 @@
 with pkgs;
 
 let
-  inherit (lib) optional optionals;
-
   basePackages = [
-    nodejs
+    git
+    nodejs-14_x
+    # the default yarn uses the current version of nodejs in <nixpkgs> so we need to tell it to use an older version
+    (yarn.override { nodejs = nodejs-14_x; })
     yarn
   ];
 
   inputs = basePackages;
 in
-
-mkShell {
-  buildInputs = inputs;
-}
-
+  mkShell {
+    buildInputs = inputs;
+  }

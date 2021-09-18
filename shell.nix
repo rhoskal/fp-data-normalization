77424e1ed4e52f94d04512a4073c0706e72c70c0
Hans Hoffman <hoffman.hans.j@gmail.com>
2021-09-18 13:27:51 -0400

    feat: add niv to manage nix deps

--- shell.nix
+++ shell.nix
@@ -1,25 +1,16 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-with pkgs;
-
 let
-  inherit (lib) optionals;
+  sources = import ./nix/sources.nix { };
+  pkgs = import sources.nixpkgs { };
+  inherit (pkgs.lib) optionals;
 
-  basePackages = [
-    git
-    nixfmt
-    nodejs-14_x
-    # the default yarn uses the current version of nodejs in <nixpkgs> so we need to tell it to use an older version
-    (yarn.override { nodejs = nodejs-14_x; })
-    yarn
-  ];
+  basePackages = [ pkgs.git pkgs.nixfmt pkgs.nodejs pkgs.yarn ];
 
-  inputs = basePackages ++ optionals stdenv.isLinux inotify-tools
-    ++ optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
-      # https://github.com/NixOS/nixpkgs/blob/master/pkgs/os-specific/darwin/apple-sdk/frameworks.nix
-      CoreFoundation
-      CoreServices
-    ]);
+  inputs = basePackages ++ optionals pkgs.stdenv.isLinux pkgs.inotify-tools
+  ++ optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
+    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/os-specific/darwin/apple-sdk/frameworks.nix
+    CoreFoundation
+    CoreServices
+  ]);
 
   hooks = ''
     mkdir -p .nix-node
@@ -27,7 +18,7 @@ let
     export NPM_CONFIG_PREFIX=$PWD/.nix-node
     export PATH=$NODE_PATH/bin:$PATH
   '';
-in mkShell {
+in pkgs.mkShell {
   buildInputs = inputs;
   shellHook = hooks;
 }
